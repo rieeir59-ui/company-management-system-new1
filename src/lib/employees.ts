944cf97349f49138e076e66ab40b46adfeec2495
RIE_EIR <rieeir59@gmail.com>
2025-12-10 05:46:23 +0000

    luqman , haseeb ,waqas rasool , noman  inky bi multiple department do ,a

--- src/lib/employees.ts
+++ src/lib/employees.ts
@@ -16,17 +16,17 @@ export const employees: Employee[] = [
   { uid: 'user3', name: 'Fiza', contact: '0306-5081954', email: 'fiza@ri-hub.com', record: 'EMP-003', avatarId: 'avatar-1', departments: ['hr'], password: 'password' },
   { uid: 'user4', name: 'Rabiya Eman', contact: '0302-2774442', email: 'rabiya.eman@ri-hub.com', record: 'EMP-004', avatarId: 'avatar-1', departments: ['software-engineer'], password: 'password' },
   { uid: 'user5', name: 'Imran Abbas', contact: '0325-5382699', email: 'imran.abbas@ri-hub.com', record: 'EMP-005', avatarId: 'avatar-2', departments: ['software-engineer'], password: 'password' },
-  { uid: 'user6', name: 'Waqas Rasool', contact: '0321-5564674', email: 'waqas.rasool@ri-hub.com', record: 'EMP-006', avatarId: 'avatar-2', departments: ['finance'], password: 'password' },
+  { uid: 'user6', name: 'Waqas Rasool', contact: '0321-5564674', email: 'waqas.rasool@ri-hub.com', record: 'EMP-006', avatarId: 'avatar-2', departments: ['finance', 'admin'], password: 'password' },
   { uid: 'user7', name: 'Mujahid', contact: '0300-4741406', email: 'mujahid@ri-hub.com', record: 'EMP-007', avatarId: 'avatar-2', departments: ['draftpersons'], password: 'password' },
   { uid: 'user8', name: 'Jabbar', contact: '0333-4624328', email: 'jabbar@ri-hub.com', record: 'EMP-008', avatarId: 'avatar-2', departments: ['draftpersons'], password: 'password' },
   { uid: 'user9', name: 'Mohsin', contact: '0321-8846995', email: 'mohsin@ri-hub.com', record: 'EMP-009', avatarId: 'avatar-2', departments: ['3d-visualizer'], password: 'password' },
-  { uid: 'user10', name: 'Haseeb', contact: '0321-9400570', email: 'haseeb@ri-hub.com', record: 'EMP-010', avatarId: 'avatar-2', departments: ['architects'], password: 'password' },
-  { uid: 'user11', name: 'Luqman', contact: '0321-1111261', email: 'luqman@ri-hub.com', record: 'EMP-011', avatarId: 'avatar-2', departments: ['architects'], password: 'password' },
+  { uid: 'user10', name: 'Haseeb', contact: '0321-9400570', email: 'haseeb@ri-hub.com', record: 'EMP-010', avatarId: 'avatar-2', departments: ['architects', 'admin'], password: 'password' },
+  { uid: 'user11', name: 'Luqman', contact: '0321-1111261', email: 'luqman@ri-hub.com', record: 'EMP-011', avatarId: 'avatar-2', departments: ['architects', 'admin'], password: 'password' },
   { uid: 'user12', name: 'Asad', contact: '0321-4333215', email: 'asad@ri-hub.com', record: 'EMP-012', avatarId: 'avatar-2', departments: ['architects', 'admin'], password: 'password' },
   { uid: 'user13', name: 'Waleed', contact: '0332-0424458', email: 'waleed@ri-hub.com', record: 'EMP-013', avatarId: 'avatar-2', departments: ['architects'], password: 'password' },
   { uid: 'user14', name: 'Kizzar', contact: '0313-9592679', email: 'kizzar@ri-hub.com', record: 'EMP-014', avatarId: 'avatar-2', departments: ['architects'], password: 'password' },
   { uid: 'user15', name: 'Waqas', contact: '0321-8404780', email: 'waqas@ri-hub.com', record: 'EMP-015', avatarId: 'avatar-2', departments: ['draftpersons'], password: 'password' },
-  { uid: 'user16', name: 'Noman', contact: '0302-8499301', email: 'noman@ri-hub.com', record: 'EMP-016', avatarId: 'avatar-2', departments: ['quantity-management'], password: 'password' },
+  { uid: 'user16', name: 'Noman', contact: '0302-8499301', email: 'noman@ri-hub.com', record: 'EMP-016', avatarId: 'avatar-2', departments: ['quantity-management', 'admin'], password: 'password' },
 ];
 
 export const employeesByDepartment = employees.reduce((acc, employee) => {
